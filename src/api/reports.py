2b073bfad4897ef5eda35b39d97ec73fdf8f9d05
jjcorderomejia <jjcorderomejia@gmail.com>
2023-03-09 22:52:25 -0500

    new commit

--- src/api/reports.py
+++ src/api/reports.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -62,11 +63,16 @@ def employee_count():
     '''
     cur.execute(query)
     result = cur.fetchall()
-    table = "| Department | Job | Q1 | Q2 | Q3 | Q4 |\n| --- | --- | --- | --- | --- | --- |\n"
-    for row in result:
-        table += f"| {row[0]} | {row[1]} | {row[2]} | {row[3]} | {row[4]} | {row[5]} |\n"
     cur.close()
     conn.close()
+
+    table = "<table><thead><tr><th>Department</th><th>Job</th><th>Q1</th><th>Q2</th><th>Q3</th><th>Q4</th></tr></thead><tbody>"
+
+    for row in result:
+        table += f"<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td><td>{row[3]}</td><td>{row[4]}</td><td>{row[5]}</td></tr>"
+
+    table += "</tbody></table>"
+
     return table, 200
 
 
@@ -92,13 +98,50 @@ def hiring():
     """
     cur.execute(query)
     result = cur.fetchall()
-    response = []
+
+    table = "<table><thead><tr><th>Department ID</th><th>Department</th><th>Number of Hires</th></tr></thead><tbody>"
     for row in result:
-        response.append({'id': row[0], 'department': row[1], 'hired': row[2]})
+        table += f"<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td></tr>"
+    table += "</tbody></table>"
+
     cur.close()
     conn.close()
-    return response, 200
+    return table, 200
 
+
+@app.route('/hiring_graph', methods=['GET'])
+def hiring_graph():
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USERNAME,
+        password=DB_PASSWORD
+    )
+    cur = conn.cursor()
+
+    query = """
+    SELECT d.department, COUNT(*) AS hired
+    FROM hired_employees he
+    JOIN departments d ON d.id = he.department_id
+    WHERE DATE_TRUNC('year', he.datetime) = '2021-01-01'
+    GROUP BY d.department
+    HAVING COUNT(*) > (SELECT AVG(cnt) FROM (SELECT COUNT(*) AS cnt FROM hired_employees GROUP BY department_id) AS sub)
+    ORDER BY hired DESC;
+    """
+    cur.execute(query)
+    result = cur.fetchall()
+
+    data = {"departments": [], "hires": []}
+    for row in result:
+        data["departments"].append(row[0])
+        data["hires"].append(row[1])
+    chart_data = json.dumps(data)
+
+    cur.close()
+    conn.close()
+
+    return render_template('hiring_graph.html', chart_data=chart_data)
 
 
 if __name__ == '__main__':
